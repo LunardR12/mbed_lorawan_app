ab40f837b29eeec67490962808ef61086b3cf507
BENAZZOUZ Ahmed <abenazzouz15@gmail.com>
2023-11-29 15:24:42 +0100

    Ajout LoRaWAN_class.hpp + modif LoRaWAN_class.cpp

---
+++ LoRaWAN_class.cpp
@@ -0,0 +1,198 @@
+#include "LoRaWAN_class.hpp"
+
+
+/**
+* Sends a message to the Network Server
+*/
+static void send_message()
+{
+
+
+    int16_t retcode;
+    char sensor_value[] = "{\"dust_sensor\": %d}";
+    uint16_t packet_len = strlen(sensor_value);
+
+    using namespace sixtron;
+
+    HPMA115 dust_sensor(P1_UART_TX, P1_UART_RX);
+    hpma115_data_t * value;
+    dust_sensor.start_measurement();
+    dust_sensor.read_measurement (value);
+
+
+    // **********************************************
+
+    packet_len = sprintf((char *) tx_buffer, "{\"dust_sensor\": %d}",
+                        value->pm1_0);
+
+    retcode = lorawan.send(MBED_CONF_LORA_APP_PORT, tx_buffer, packet_len,
+                        MSG_UNCONFIRMED_FLAG);
+
+    if (retcode < 0) {
+        retcode == LORAWAN_STATUS_WOULD_BLOCK ? printf("send - WOULD BLOCK\r\n")
+        : printf("\r\n send() - Error code %d \r\n", retcode);
+
+        if (retcode == LORAWAN_STATUS_WOULD_BLOCK) {
+            //retry in 3 seconds
+            if (MBED_CONF_LORA_DUTY_CYCLE_ON) {
+                ev_queue.call_in(3000, send_message);
+            }
+        }
+        return;
+    }
+
+    printf("\r\n %d bytes scheduled for transmission \r\n", retcode);
+    memset(tx_buffer, 0, sizeof(tx_buffer));
+}
+
+/**
+* Receive a message from the Network Server
+*/
+static void receive_message()
+{
+    uint8_t port;
+    int flags;
+    int16_t retcode = lorawan.receive(rx_buffer, sizeof(rx_buffer), port, flags);
+
+    if (retcode < 0) {
+        printf("\r\n receive() - Error code %d \r\n", retcode);
+        return;
+    }
+
+    printf(" RX Data on port %u (%d bytes): ", port, retcode);
+    for (uint8_t i = 0; i < retcode; i++) {
+        printf("%02x ", rx_buffer[i]);
+    }
+    printf("\r\n");
+    
+    memset(rx_buffer, 0, sizeof(rx_buffer));
+}
+
+/**
+* Event handler
+*/
+static void lora_event_handler(lorawan_event_t event)
+{
+    switch (event) {
+        case CONNECTED:
+            printf("\r\n Connection - Successful \r\n");
+            if (MBED_CONF_LORA_DUTY_CYCLE_ON) {
+                send_message();
+            } else {
+                ev_queue.call_every(TX_TIMER, send_message);
+            }
+
+            break;
+        case DISCONNECTED:
+            ev_queue.break_dispatch();
+            printf("\r\n Disconnected Successfully \r\n");
+            break;
+        case TX_DONE:
+            printf("\r\n Message Sent to Network Server \r\n");
+            if (MBED_CONF_LORA_DUTY_CYCLE_ON) {
+                send_message();
+            }
+            break;
+        case TX_TIMEOUT:
+        case TX_ERROR:
+        case TX_CRYPTO_ERROR:
+        case TX_SCHEDULING_ERROR:
+            printf("\r\n Transmission Error - EventCode = %d \r\n", event);
+            // try again
+            if (MBED_CONF_LORA_DUTY_CYCLE_ON) {
+                send_message();
+            }
+            break;
+        case RX_DONE:
+            printf("\r\n Received message from Network Server \r\n");
+            receive_message();
+            break;
+        case RX_TIMEOUT:
+        case RX_ERROR:
+            printf("\r\n Error in reception - Code = %d \r\n", event);
+            break;
+        case JOIN_FAILURE:
+            printf("\r\n OTAA Failed - Check Keys \r\n");
+            break;
+        case UPLINK_REQUIRED:
+            printf("\r\n Uplink required by NS \r\n");
+            if (MBED_CONF_LORA_DUTY_CYCLE_ON) {
+                send_message();
+            }
+            break;
+        default:
+            MBED_ASSERT("Unknown Event");
+    }
+}
+
+void Init() {
+
+    // setup tracing
+    setup_trace();
+
+    // Initialize LoRaWAN stack
+    if (lorawan.initialize(&ev_queue) != LORAWAN_STATUS_OK) {
+        printf("\r\n LoRa initialization failed! \r\n");
+        return -1;
+    }
+
+    printf("\r\n Mbed LoRaWANStack initialized \r\n");
+
+    // prepare application callbacks
+    callbacks.events = mbed::callback(lora_event_handler);
+    lorawan.add_app_callbacks(&callbacks);
+
+    // Set number of retries in case of CONFIRMED messages
+    if (lorawan.set_confirmed_msg_retries(CONFIRMED_MSG_RETRY_COUNTER)
+            != LORAWAN_STATUS_OK) {
+        printf("\r\n set_confirmed_msg_retries failed! \r\n\r\n");
+        return -1;
+    }
+
+    printf("\r\n CONFIRMED message retries : %d \r\n",
+        CONFIRMED_MSG_RETRY_COUNTER);
+
+    // Enable adaptive data rate
+    if (lorawan.enable_adaptive_datarate() != LORAWAN_STATUS_OK) {
+        printf("\r\n enable_adaptive_datarate failed! \r\n");
+        return -1;
+    }
+
+    printf("\r\n Adaptive data  rate (ADR) - Enabled \r\n");
+}
+
+void Connect() {
+    retcode = lorawan.connect();
+
+    if (retcode == LORAWAN_STATUS_OK ||
+            retcode == LORAWAN_STATUS_CONNECT_IN_PROGRESS) {
+    } else {
+        printf("\r\n Connection error, code = %d \r\n", retcode);
+        return -1;
+    }
+
+    printf("\r\n Connection - In Progress ...\r\n");
+
+    // make your event queue dispatching events forever
+    ev_queue.dispatch_forever();
+
+    return 0;
+}
+
+void Disconnect() {
+    retcode = lorawan.Disconnect();
+
+    if (retcode == LORAWAN_STATUS_OK ||
+            retcode == LORAWAN_STATUS_CONNECT_IN_PROGRESS) { // 
+    } else {
+        printf("\r\n Disconnection error, code = %d \r\n", retcode);
+        return -1;
+    }
+
+    printf("\r\n Disconnection - In Progress ...\r\n");
+
+    // make your event queue dispatching events forever
+    ev_queue.dispatch_forever();
+
+    return 0;
+}
---
+++ LoRaWAN_class.hpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+#include "lorawan/LoRaWANInterface.h"
+#include "lorawan/system/lorawan_data_structures.h"
+#include "events/EventQueue.h"
+
+// Application helpers
+#include "./mbed_honeywell-hpma115/hpma115/hpma115.h"
+#include "trace_helper.h"
+#include "lora_radio_helper.h"
+#include <PinNames.h>
+
+ using namespace events;
+ 
+// Define a template class
+template <typename T>
+
+class LoRaWAN_app {
+
+private:
+
+    // Max payload size can be LORAMAC_PHY_MAXPAYLOAD.
+    // This example only communicates with much shorter messages (<30 bytes).
+    // If longer messages are used, these buffers must be changed accordingly.
+    uint8_t tx_buffer[30];
+    uint8_t rx_buffer[30];
+
+    /*
+    * Sets up an application dependent transmission timer in ms. Used only when Duty Cycling is off for testing
+    */
+    #define TX_TIMER                        10000
+
+    /**
+    * Maximum number of events for the event queue.
+    * 10 is the safe number for the stack events, however, if application
+    * also uses the queue for whatever purposes, this number should be increased.
+    */
+    #define MAX_NUMBER_OF_EVENTS            10
+
+    /**
+    * Maximum number of retries for CONFIRMED messages before giving up
+    */
+    #define CONFIRMED_MSG_RETRY_COUNTER     3
+
+    /**
+    * Application specific callbacks
+    */
+    static lorawan_app_callbacks_t callbacks;
+
+    /**
+    * Constructing Mbed LoRaWANInterface and passing it the radio object from lora_radio_helper.
+    */
+    static LoRaWANInterface lorawan(radio);
+
+    /**
+    * This event queue is the global event queue for both the
+    * application and stack. To conserve memory, the stack is designed to run
+    * in the same thread as the application and the application is responsible for
+    * providing an event queue to the stack that will be used for ISR deferment as
+    * well as application information event queuing.
+    */
+    static EventQueue ev_queue(MAX_NUMBER_OF_EVENTS *EVENTS_EVENT_SIZE)
+
+    /**
+    * Event handler.
+    *
+    * This will be passed to the LoRaWAN stack to queue events for the
+    * application which in turn drive the application.
+    */
+    static void lora_event_handler(lorawan_event_t event);
+
+    // stores the status of a call to LoRaWAN protocol
+    lorawan_status_t retcode;
+
+    /**
+    * Sends a message to the Network Server
+    */
+    static void send_message();
+
+    /**
+    * Receive a message from the Network Server
+    */
+    static void receive_message();
+
+    /**
+    * Event handler
+    */
+    static void lora_event_handler(lorawan_event_t event);
+
+public:
+    // Constructor
+    MyClass(T value) : data(value) {}
+
+    // Member function
+    void Init();
+
+    void Connect();
+
+    void Disconnect();
+
+};
